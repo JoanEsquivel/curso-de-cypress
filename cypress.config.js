9927f75e981ddc5f737d801a918eaf4bb9a778f9
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2025-01-04 15:43:45 -0600

    feat: reporteria

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Curso de Cypress by JoanMedia',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    autoOpen: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
